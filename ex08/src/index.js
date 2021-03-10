5569f25be8ee4b21c28600b5f057d8a05214c737
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-10 23:29:35 +0100

    Solution

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,12 +1,16 @@
-function main() {
+function main(numOne, numTwo) {
     // Only change code below this line
-    var numOne = 15;
-    var numTwo = 8;
+    
+    var numOne = numOne;
+    var numTwo = numTwo;
     var sum = numOne + numTwo;
     // Only change code below this line
+
     return sum;
     
 }
 
-console.log(main(15, 8));  
+console.log(main(15,8));
+console.log(main(5,28));
+console.log(main(150,425));
 module.exports = main;
