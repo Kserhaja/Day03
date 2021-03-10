b8f54e9c0b20e1c1f0b21d701bb44da5da1465f1
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-10 22:28:50 +0100

    new changes

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -2,7 +2,7 @@ function main() {
     // Only change code below this line
     var numOne = 15;
     var numTwo = 8;
-    var sum = 15 + 8;
+    var sum = numOne + numTwo;
     // Only change code below this line
     return sum;
        
