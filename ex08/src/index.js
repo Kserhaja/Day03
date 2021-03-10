f7ab21409d83d134a4ea38c3e8340d5b520a486e
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-10 23:08:07 +0100

    update 13

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,13 +1,21 @@
 function main() {
     // Only change code below this line
+    var numOne = 15;
+    var numTwo = 8;
+    var sum = numOne + numTwo;
+    // Only change code below this line
+    return sum;
+    
+}
+{
+    // Only change code below this line
     var numOne = 5;
-    var numTwo = 23;
+    var numTwo = 28;
     var sum = numOne + numTwo;
     // Only change code below this line
     return sum;
     
 }
 
-
-console.log(main(15, 8));  
+console.log(main(numOne, numTwo));  
 module.exports = main;
